73c1cbcbfc1aeba52f83545ab9949d4404c61c8d
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2024-04-25 15:19:24 +0100

    added functionality to allow users to vote on a poll

--- src/BasicVotingPoll.sol
+++ src/BasicVotingPoll.sol
@@ -16,6 +16,7 @@ contract BasicVotingPoll {
     mapping(address=>bool) public s_administrators;
     mapping(address=>bool) public s_registeredUsers;
     mapping(uint256=>Poll) public s_polls;
+    mapping(uint256=>mapping(address=>uint)) public s_pollVotes;
 
     uint256 public s_pollCount;
 
@@ -25,7 +26,7 @@ contract BasicVotingPoll {
         string[] votingOptions;
         uint256 votingDuration;
         uint256 startAt;
-        bool openedToRegisteredUsers;
+        bool openedToOtherUsers;
     }
 
     /// @notice This function allows admins to create a poll
@@ -33,12 +34,12 @@ contract BasicVotingPoll {
     /// @param _question The central query or subject matter that users will be voting on. 
     /// @param _votingOptions The possible choices or answers available for users to select when casting their votes.
     /// @param _votingDuration The designated period during which users can participate in the voting process.
-    /// @param _openedToRegisteredUsers Whether or not un-registered users can vote
+    /// @param _openedToOtherUsers Whether or not un-registered users can vote
     function createPoll(
         string memory _question, 
         string[] memory _votingOptions, 
         uint256 _votingDuration,
-        bool _openedToRegisteredUsers
+        bool _openedToOtherUsers
     )external {
         require(s_administrators[msg.sender], "BasicVotingPoll: Only admins can create a poll");
         Poll memory poll = Poll({
@@ -47,14 +48,35 @@ contract BasicVotingPoll {
             votingOptions: _votingOptions,
             startAt: block.timestamp,
             votingDuration: _votingDuration,
-            openedToRegisteredUsers: _openedToRegisteredUsers
+            openedToOtherUsers: _openedToOtherUsers
         });
 
         s_polls[s_pollCount] = poll;
         s_pollCount++;
     }
 
-    function submitVote()external {}
+    /// @notice Allows users to vote in a poll
+    /// @param _pollId Id of the poll
+    /// @param _votingOption Option to vote for
+    function submitVote(
+        uint256 _pollId,
+        uint256 _votingOption
+    )external validatePollId(_pollId){
+        Poll memory poll = s_polls[_pollId];
+    
+        require(block.timestamp >= poll.startAt && block.timestamp < poll.startAt + poll.votingDuration,
+                "BasicVotingPoll: Poll has ended, cannot submit vote");
+        if(!poll.openedToOtherUsers){
+            require(s_registeredUsers[msg.sender], "BasicVotingPoll: Only registered users can vote");
+        }
+        s_pollVotes[_pollId][msg.sender] = _votingOption;
+    }
 
     function retrieveVotingResult() external {}
+
+    modifier validatePollId(uint256 _pollId){
+        require(_pollId < s_pollCount, "BasicVotingPoll: Invalid PollId");
+        require(s_polls[_pollId].startAt != 0, "BasicVotingPoll: This poll is inactive or has ended");
+        _;
+    }
 }
